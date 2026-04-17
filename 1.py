0665a3b62af28db7cd518ead4897c67181331f2a
voidaditya <dypatil.aditya00@gmail.com>
2026-04-17 17:47:21 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,23 @@
+x=4
+print(x) 
+print("Hello World!", end="")
+print("I will print on the same line.")
+
+name="aditya"
+y,z=34,44.5
+print(y,z)
+print(name)
+
+a=b=c=33
+print(c)
+
+fruits = ["apple", "banana", "cherry"]
+x1, y1, z1 = fruits
+print(x1,y1,z1)
+
+x2 = "Python "
+y2= "is "
+z2= "awesome"
+print(x2 + y2 + z2)
+
+print(y+z)
