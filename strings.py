ffb772ae8ec2b6a225d13a14341dcb833de3c6a9
voidaditya <dypatil.aditya00@gmail.com>
2026-04-17 21:50:51 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,45 @@
+x = 4
+size = x.bit_length()
+print(size)
+y=int(3)
+print (type(y))
+
+
+
+a = """my name is aditya,
+i am studing engineering"""
+print(a)
+print(a.upper())
+
+
+
+#slicing
+b = "Hello, World!"
+print(b[:12])
+a = "Hello, World!"
+print(b.lower())
+
+
+c = "Hello, World!"
+print(c.replace("H", "J"))
+
+
+
+print(c.split(","))
+
+#merge
+d = "Hello"
+e = "World"
+f = d + " " + e
+print(f)
+
+#format 
+age = 36
+txt = f"My name is aditya, I am {age}"
+print(txt)
+
+
+name="aditya"
+username="enter your username"
+print(username +" "+ name)
+
